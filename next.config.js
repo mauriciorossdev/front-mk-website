02f668bf0d9a20d15098bd70b495e6af77733781
nullPointer.Ross <mauriciorossdev@gmail.com>
2023-11-28 01:16:38 -0300

    Add new files and update configurations

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['localhost'],
-      },
-}
+  images: {
+    domains: ["localhost"],
+  },
+  
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
